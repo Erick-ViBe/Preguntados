6b4a8447421ee27bf34ae769523a81328d3c8a07
Erick-ViBe <sergio.erick.vicencio.benitez@gmail.com>
2021-12-13 19:33:28 -0600

    update

--- Preguntados/questions/urls.py
+++ Preguntados/questions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, MyProfileView, QuestionDetailView
+from .views import HomeView, MyProfileView, QuestionDetailView, QuestionCreateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home-view'),
     path('my_asks/', MyProfileView.as_view(), name='my_asks-view'),
+    path('new_question/', QuestionCreateView.as_view(), name='question-create'),
     path('question/<int:question_id>/', QuestionDetailView.as_view(), name='question-detail')
 ]
--- Preguntados/questions/views.py
+++ Preguntados/questions/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Question
+from django.urls import reverse
+from .models import Question, Comment
+from .forms import QuestionForm
 
 
 class HomeView(LoginRequiredMixin, View):
@@ -32,18 +34,13 @@ class MyProfileView(LoginRequiredMixin, View):
         return render(request, self.template_name, self.get_context())
 
 
-class QuestionDetailView(LoginRequiredMixin, View):
-    template_name = 'questions/question_detail.html'
-    question_id = None
-
-    def dispatch(self, request, *args, **kwargs):
-        self.question_id = kwargs['question_id']
-        return super().dispatch(request, *args, **kwargs)
+class QuestionCreateView(LoginRequiredMixin, View):
+    template_name = 'questions/question_create.html'
+    form_class = QuestionForm
 
     def get_context(self):
-        question = get_object_or_404(Question, pk=self.question_id)
         context = {
-            'question': question
+            'form': self.form_class,
         }
         return context
 
@@ -51,4 +48,33 @@ class QuestionDetailView(LoginRequiredMixin, View):
         return render(request, self.template_name, self.get_context())
 
     def post(self, request, *args, **kwargs):
-        print(f'El user {request.user} intento enviar algo')
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.save()
+        return redirect(reverse('home-view'))
+
+
+
+class QuestionDetailView(LoginRequiredMixin, View):
+    template_name = 'questions/question_detail.html'
+    question = None
+
+    def dispatch(self, request, *args, **kwargs):
+        self.question = get_object_or_404(Question, pk=kwargs['question_id'])
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context(self):
+        context = {
+            'question': self.question
+        }
+        return context
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.get_context())
+
+    def post(self, request, *args, **kwargs):
+        new_comment = request.POST.get('new_comment')
+        Comment.objects.create(question=self.question, author=request.user, content=new_comment)
+        return self.get(request)
